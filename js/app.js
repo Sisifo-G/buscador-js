7c39ae906cd5aea5b4ca3cdcdaedae25c677c549
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-06 17:19:22 -0500

    Mostrando mensaje de error

--- js/app.js
+++ js/app.js
@@ -47,22 +47,27 @@ year.addEventListener('change', e =>{
 
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 });
 
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
 });
 
 puertas.addEventListener('change', e => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
 });
 
 transmision.addEventListener('change', e => {
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 })
 
 color.addEventListener('change', e => {
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
 });
 
 
@@ -104,8 +109,22 @@ function limpiarHTML(){
 // Función que filtra en base a los criterios de búsqueda
 
 function filtrarAuto(){
-    const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
-    mostrarAutos(resultado);
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor)
+    
+    if(resultado.length){
+        mostrarAutos(resultado);
+
+    }else{
+        noResultado();
+    }
+}
+
+function noResultado(){
+    limpiarHTML();
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados';
+    resultado.appendChild(noResultado);
 }
 
 function filtrarMarca(auto){
@@ -123,3 +142,44 @@ function filtrarYear(auto){
     }
     return auto;
 }
+
+function filtrarMinimo (auto){
+    const { minimo } = datosBusqueda;
+    if(minimo){
+        return auto.precio >=minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto){
+    const { maximo } = datosBusqueda;
+    if(maximo){
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+
+function filtrarPuertas(auto){
+    const { puertas } = datosBusqueda;
+    if(puertas){
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto){
+    const { transmision } = datosBusqueda;
+    if(transmision){
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto){
+    const { color } = datosBusqueda;
+    if(color){
+        return auto.color === color;
+    }
+    return auto;
+}
+
